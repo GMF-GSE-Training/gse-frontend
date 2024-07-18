e6b808d39cd2256065aa79d35f30ccd7244e2ef2
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-07-18 23:09:48 +0700

    Membuat komponen nav-menu untuk menu navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SearchComponent } from './search/search.component';
+import { NavMenuComponent } from "./nav-menu/nav-menu.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     RouterOutlet,
-    SearchComponent
-  ],
+    SearchComponent,
+    NavMenuComponent
+],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
