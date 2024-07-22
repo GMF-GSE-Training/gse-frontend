499eba9c6cfded7130f3a81637f804d10941f4b2
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-07-23 01:21:40 +0700

    menambahkan routerLink ke /home di komponen view capability

--- src/app/view-capability/view-capability.component.ts
+++ src/app/view-capability/view-capability.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-view-capability',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    RouterLink
   ],
   templateUrl: './view-capability.component.html',
   styleUrl: './view-capability.component.css'
