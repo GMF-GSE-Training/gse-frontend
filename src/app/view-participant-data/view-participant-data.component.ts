c031ea5ce806f753531e9dd37aa1ff8a3d942833
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-07-23 01:02:07 +0700

    menambahkan isi konten utama pada view participant data

--- src/app/view-participant-data/view-participant-data.component.ts
+++ src/app/view-participant-data/view-participant-data.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-view-participant-data',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    RouterLink
   ],
   templateUrl: './view-participant-data.component.html',
   styleUrl: './view-participant-data.component.css'
