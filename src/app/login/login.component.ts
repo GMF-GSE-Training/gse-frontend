ba575515bf4207f11d89bfa1358151f5c7ef6ae2
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-07-23 22:31:40 +0700

    menerapkan blue-button ke halaman login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { BlueButtonComponent } from '../component/blue-button/blue-button.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    BlueButtonComponent
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
