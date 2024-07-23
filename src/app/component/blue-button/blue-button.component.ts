4d0c0f29d8c2d1249bf04d2fdf84f9c297c6d29a
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-07-23 22:15:29 +0700

    menambahkan komponen tombol berwarna biru

---
+++ src/app/component/blue-button/blue-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blue-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './blue-button.component.html',
+  styleUrl: './blue-button.component.css'
+})
+export class BlueButtonComponent {
+
+}
