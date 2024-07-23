1e7a41e26af660ba1e1b2cc41421551b8737f2ad
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-07-24 01:04:56 +0700

    menerapkan blue button ke tombol back di detail participant data

--- src/app/detail-participant-data/detail-participant-data.component.ts
+++ src/app/detail-participant-data/detail-participant-data.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { BlueButtonComponent } from "../component/blue-button/blue-button.component";
 
 @Component({
   selector: 'app-detail-participant-data',
   standalone: true,
   imports: [
     RouterLink,
-    NavbarComponent
-  ],
+    NavbarComponent,
+    BlueButtonComponent
+],
   templateUrl: './detail-participant-data.component.html',
   styleUrl: './detail-participant-data.component.css'
 })
