a7888b40fd54b3df346fa1ed818996ec99389cfd
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-07-23 11:48:39 +0700

    memberikan routing ke halaman home pada tombol back

--- src/app/add-participant-data/add-participant-data.component.ts
+++ src/app/add-participant-data/add-participant-data.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-add-participant-data',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [
+    RouterLink,
+    NavbarComponent
+  ],
   templateUrl: './add-participant-data.component.html',
   styleUrl: './add-participant-data.component.css'
 })
