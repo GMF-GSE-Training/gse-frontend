4d0b8f8848261fd9f9ec7eab92e4500eebee571c
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-07-23 22:35:34 +0700

    menerapkan blue button ke halaman add participant data

--- src/app/add-participant-data/add-participant-data.component.ts
+++ src/app/add-participant-data/add-participant-data.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { RouterLink } from '@angular/router';
+import { BlueButtonComponent } from "../component/blue-button/blue-button.component";
 
 @Component({
   selector: 'app-add-participant-data',
   standalone: true,
   imports: [
     RouterLink,
-    NavbarComponent
-  ],
+    NavbarComponent,
+    BlueButtonComponent
+],
   templateUrl: './add-participant-data.component.html',
   styleUrl: './add-participant-data.component.css'
 })
