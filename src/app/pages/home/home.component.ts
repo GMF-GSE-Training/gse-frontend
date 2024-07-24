232556a10ad15f3e6d74e1f523237ab244356203
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-07-24 16:57:19 +0700

    memindahkan semua komponen grafik ke direktori /component/chart

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../component/navbar/navbar.component';
-import { RealisasiCotChartComponent } from './realisasi-cot-chart/realisasi-cot-chart.component';
-import { DataPemegangKompetensiGseOperatorComponent } from './data-pemegang-kompetensi-gse-operator/data-pemegang-kompetensi-gse-operator.component';
-import { DataTotalSertifikatAktifComponent } from './data-total-sertifikat-aktif/data-total-sertifikat-aktif.component';
-import { DataJumlahPemegangSertifikatComponent } from './data-jumlah-pemegang-sertifikat/data-jumlah-pemegang-sertifikat.component';
+import { RealisasiCotChartComponent } from '../../component/chart/realisasi-cot-chart/realisasi-cot-chart.component';
+import { DataPemegangKompetensiGseOperatorComponent } from '../../component/chart/data-pemegang-kompetensi-gse-operator/data-pemegang-kompetensi-gse-operator.component';
+import { DataTotalSertifikatAktifComponent } from '../../component/chart/data-total-sertifikat-aktif/data-total-sertifikat-aktif.component';
+import { DataJumlahPemegangSertifikatComponent } from '../../component/chart/data-jumlah-pemegang-sertifikat/data-jumlah-pemegang-sertifikat.component';
 import { CotTableComponent } from './cot-table/cot-table.component';
 
 @Component({
