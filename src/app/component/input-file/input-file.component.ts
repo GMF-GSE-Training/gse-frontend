91923b714f41d6d4a151c6f41eda644cfa8e9007
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-07-24 14:25:22 +0700

    membuat komponen input file

--- src/app/component/input-file/input-file.component.ts
+++ src/app/component/input-file/input-file.component.ts
@@ -5,8 +5,17 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './input-file.component.html',
-  styleUrl: './input-file.component.css'
+  styleUrl: './input-file.component.css',
 })
 export class InputFileComponent {
+  fileName: string | null = null;
 
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.fileName = input.files[0].name;
+    } else {
+      this.fileName = null;
+    }
+  }
 }
