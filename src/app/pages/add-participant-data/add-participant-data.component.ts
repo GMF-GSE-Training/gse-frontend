6fe77be66ffac30c34d3dace08e4c50cf7a243c2
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-07-24 14:27:41 +0700

    menerapkan komponen input file pada halaman add participant data

--- src/app/pages/add-participant-data/add-participant-data.component.ts
+++ src/app/pages/add-participant-data/add-participant-data.component.ts
@@ -3,6 +3,7 @@ import { NavbarComponent } from '../../navbar/navbar.component';
 import { RouterLink } from '@angular/router';
 import { WhiteButtonComponent } from '../../component/white-button/white-button.component';
 import { BlueButtonComponent } from '../../component/blue-button/blue-button.component';
+import { InputFileComponent } from "../../component/input-file/input-file.component";
 
 @Component({
   selector: 'app-add-participant-data',
@@ -11,7 +12,8 @@ import { BlueButtonComponent } from '../../component/blue-button/blue-button.com
     RouterLink,
     NavbarComponent,
     BlueButtonComponent,
-    WhiteButtonComponent
+    WhiteButtonComponent,
+    InputFileComponent
 ],
   templateUrl: './add-participant-data.component.html',
   styleUrl: './add-participant-data.component.css'
