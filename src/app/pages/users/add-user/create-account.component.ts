0604a022211c68866209786853ace43dd6b1a7df
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-09-05 19:41:33 +0700

    memperbaiki validasi data yang dikirim ke server

--- src/app/pages/users/add-user/create-account.component.ts
+++ src/app/pages/users/add-user/create-account.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { BlueButtonComponent } from "../../../component/button/blue-button/blue-button.component";
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { WhiteButtonComponent } from '../../../component/button/white-button/white-button.component';
 import { RoleBasedAccessDirective } from '../../../shared/directive/role-based-access.directive';
 import { LoginRegisterComponent } from "../../../component/login-register/login-register.component";
@@ -41,6 +41,7 @@ export class AddUserComponent {
   roles: any[] = [];
 
   constructor(
+    private router: Router,
     private roleService: RoleService,
     private userService: UserService,
     private sweetalertService: SweetalertService,
@@ -65,6 +66,8 @@ export class AddUserComponent {
   }
 
   onCreate(): void {
+    this.cleanEmptyFields(this.createUser);
+
     if (this.createUser.roleId === 'user' && !this.createUser.nik) {
       alert('NIK is required for role user.');
       return;
@@ -75,6 +78,7 @@ export class AddUserComponent {
     this.userService.createUser(this.createUser).subscribe(
       async () => {
         await this.sweetalertService.alert(true, 'Ditambahkan!', 'Pengguna berhasil ditambahkan', 'success');
+        this.router.navigateByUrl('/users');
       },
       error => {
         const e = error.error.errors;
@@ -99,6 +103,14 @@ export class AddUserComponent {
         }
       }
     );
+  }
+
+  private cleanEmptyFields(object: any): void {
+    for (const key in object) {
+      if (object.hasOwnProperty(key) && object[key] === '') {
+        object[key] = null;  // Atau bisa diubah menjadi undefined
+      }
+    }
   }
 
   onEnterKey(event: Event): void {
