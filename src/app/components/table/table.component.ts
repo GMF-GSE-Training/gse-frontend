1925d8f0b4daa37b6b34b44e29daa0562a439be7
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-09-27 16:50:22 +0700

    memperbaiki pengalaman pengguna pada tombol cetak kartu peserta dan download

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -14,7 +14,7 @@ import { IconActionComponent } from "../icon-action/icon-action.component";
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
-export class TableComponent {
+export class  TableComponent {
   @Input() columns: { header: string, field: string }[] = [];
   @Input() data: any[] = [];
 }
