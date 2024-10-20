4aca661ce981c52fc221777af94d0053b0b7fef2
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-10-20 23:01:59 +0700

    melengkapi fitur capability-detail

--- src/app/pages/capability/capability-detail/capability-detail.component.ts
+++ src/app/pages/capability/capability-detail/capability-detail.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../../components/header/header.component';
 import { WhiteButtonComponent } from "../../../components/button/white-button/white-button.component";
 import { BaseInputComponent } from '../../../components/input/base-input/base-input.component';
 import { TitleComponent } from "../../../components/title/title.component";
 import { CommonModule } from '@angular/common';
 import { BlueButtonComponent } from "../../../components/button/blue-button/blue-button.component";
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { CurriculumSyllabusService } from '../../../shared/service/curriculum-syllabus.service';
-import { CreateCurriculumSyllabus } from '../../../shared/model/curriculum-syllabus.model';
+import { CapabilityService } from '../../../shared/service/capability.service';
 
 @Component({
   selector: 'app-capability-detail',
@@ -26,24 +25,20 @@ import { CreateCurriculumSyllabus } from '../../../shared/model/curriculum-sylla
   templateUrl: './capability-detail.component.html',
   styleUrl: './capability-detail.component.css'
 })
-export class CapabilityDetailComponent {
+export class CapabilityDetailComponent implements OnInit {
   capability = {
-    id:'',
+    id: '',
     kodeRating: '',
     kodeTraining: '',
     namaTraining: ''
   }
 
-  curriculumSyllabus: CreateCurriculumSyllabus = {
-    curriculumSyllabus: []
-  }
-
-  regulasiGSEs: Array<{ capabilityId: string; nama: string; durasiTeori: number; durasiPraktek: number; type: string }> = [{
+  regulasiGSEs: Array<{ capabilityId: string; nama: string; durasiTeori: number; durasiPraktek: number; type: string; }> = [{
     capabilityId: '',
     nama: '',
     durasiTeori: 0,
     durasiPraktek: 0,
-    type: 'Regulasi GSE'
+    type: '',
   }];
 
   kompetensis: Array<{ capabilityId: string; nama: string; durasiTeori: number; durasiPraktek: number; type: string }> = [{
@@ -51,61 +46,63 @@ export class CapabilityDetailComponent {
     nama: '',
     durasiTeori: 0,
     durasiPraktek: 0,
-    type: 'Kompetensi'
+    type: '',
   }];
-
-  constructor(
-    private readonly router: Router,
-    private readonly curriculumSyllabusService: CurriculumSyllabusService,
-  ) {
-    const navigation = this.router.getCurrentNavigation();
-    const state = navigation?.extras.state as { id?: string, kodeRating?: string, namaTraining?: string } | undefined;
-    console.log(state)
-
-    if (state) {
-      this.capability.id = state.id || '';
-      this.capability.kodeRating = state.kodeRating || '';
-      this.capability.namaTraining = state.namaTraining || '';
-
-      // Assign capabilityId to the initial dynamic inputs
-      this.regulasiGSEs[0].capabilityId = this.capability.id;
-      this.kompetensis[0].capabilityId = this.capability.id;
-    }
-  }
 
   inputGroup1: Array<any> = [];
   inputGroup2: Array<any> = [];
 
-  onSubmit() {
-    // Parse input group 1 (Regulasi GSEs) to ensure numbers are correct
-    this.regulasiGSEs = this.regulasiGSEs.map(item => ({
-      ...item,
-      durasiTeori: Number(item.durasiTeori),  // Konversi ke number
-      durasiPraktek: Number(item.durasiPraktek),  // Konversi ke number
-    }));
+  editLink: string = '';
 
-    // Parse input group 2 (Kompetensis) to ensure numbers are correct
-    this.kompetensis = this.kompetensis.map(item => ({
-      ...item,
-      durasiTeori: Number(item.durasiTeori),  // Konversi ke number
-      durasiPraktek: Number(item.durasiPraktek),  // Konversi ke number
-    }));
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly capabilityService: CapabilityService,
+  ) { }
 
-    // Gabungkan semua data ke dalam curriculum_syllabus
-    const curriculumSyllabusData = [
-      ...this.regulasiGSEs,
-      ...this.kompetensis
-    ];
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.capabilityService.getCapabilityById(id).subscribe({
+        next: (response) => {
+          const data = response.data;
 
-    // Panggil service untuk mengirim data ke backend
-    this.curriculumSyllabusService.createCurriculumSyllabus({
-      curriculumSyllabus: curriculumSyllabusData
-    }).subscribe(response => {
-      // Handle response
-      console.log('Curriculum & Syllabus saved successfully', response);
-    }, error => {
-      // Handle error
-      console.error('Error saving Curriculum & Syllabus', error);
-    });
+          // Update capability data
+          this.capability = {
+            id: data.id,
+            kodeRating: data.kodeRating,
+            kodeTraining: data.kodeTraining,
+            namaTraining: data.namaTraining,
+          };
+
+          // Filter and map curriculumSyllabus to regulasiGSEs and kompetensis
+          this.regulasiGSEs = data.curriculumSyllabus!
+            .filter(item => item.type === 'Regulasi GSE')
+            .map(item => ({
+              capabilityId: item.capabilityId,
+              nama: item.nama,
+              durasiTeori: item.durasiTeori,
+              durasiPraktek: item.durasiPraktek,
+              type: item.type,
+            }));
+
+          this.kompetensis = data.curriculumSyllabus!
+            .filter(item => item.type === 'Kompetensi')
+            .map(item => ({
+              capabilityId: item.capabilityId,
+              nama: item.nama,
+              durasiTeori: item.durasiTeori,
+              durasiPraktek: item.durasiPraktek,
+              type: item.type,
+            }));
+
+          console.log(this.capability, this.regulasiGSEs, this.kompetensis);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+
+      this.editLink = `/capability/${id}/edit`;
+    }
   }
 }
