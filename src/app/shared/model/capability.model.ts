bc6e2dc1b9c26bda7bcf627cc4fa5c89e791932e
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-10-20 23:06:14 +0700

    memperbaiki tampilan halaman cpability-detail dan add-capability

--- src/app/shared/model/capability.model.ts
+++ src/app/shared/model/capability.model.ts
@@ -3,3 +3,48 @@ export interface CreateCapability {
   kodeTraining: string;
   namaTraining: string;
 }
+
+export interface CapabilityResponse {
+  code: number;
+  status: string;
+  data: {
+    id: string;
+    kodeRating: string;
+    kodeTraining: string;
+    namaTraining: string;
+    totalDurasiTeoriRegGse?: string;
+    totalDurasiPraktekRegGse?: string;
+    totalDurasiTeoriKompetensi?: string;
+    totalDurasiPraktekKompetensi?: string;
+    TotalDurasi?: string;
+    curriculumSyllabus?: CurriculumSyllabus[];
+  }
+}
+
+export interface CapabilityListResponse {
+  code: number;
+  status: string;
+  data: CapabilityResponse[],
+  actions: ActionAccessRigts,
+  paging: Paging;
+}
+
+type CurriculumSyllabus = {
+  capabilityId: string;
+  nama: string;
+  durasiTeori: number;
+  durasiPraktek: number;
+  type: string;
+}
+
+type ActionAccessRigts = {
+  canEdit: boolean;
+  canDelete: boolean;
+  canView: boolean;
+}
+
+type Paging = {
+  currentPage: number;
+  totalPage: number;
+  size: number;
+}
--- src/app/shared/service/capability.service.ts
+++ src/app/shared/service/capability.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { environment } from "../../../environments/environment.development";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { CapabilityListResponse, CapabilityResponse } from "../model/capability.model";
 
 @Injectable({
   providedIn: 'root',
@@ -14,11 +15,15 @@ export class CapabilityService {
     private http: HttpClient,
   ) {}
 
-  createCapability(request: any): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/${this.endpoint.base}`, request, { withCredentials: true });
+  createCapability(request: any): Observable<CapabilityResponse> {
+    return this.http.post<CapabilityResponse>(`${this.apiUrl}/${this.endpoint.base}`, request, { withCredentials: true });
   }
 
-  listCapability(page: number = 1, size: number = 10): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/${this.endpoint.list}?page=${page}&size=${size}`, { withCredentials: true });
+  getCapabilityById(id: string): Observable<CapabilityResponse> {
+    return this.http.get<CapabilityResponse>(`${this.apiUrl}/${this.endpoint.base}/${id}`, { withCredentials: true });
+  }
+
+  listCapability(page: number = 1, size: number = 10): Observable<CapabilityListResponse> {
+    return this.http.get<CapabilityListResponse>(`${this.apiUrl}/${this.endpoint.list}?page=${page}&size=${size}`, { withCredentials: true });
   }
 }
