9ab264ad6578033e429f40456abe8b151b26052a
pangaribowo <fatahillahalifp04@gmail.com>
2025-02-18 07:07:48 +0700

    feat(deployment): add multi-stage Dockerfile, custom Nginx config, and entrypoint.sh for runtime env injection

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,18 @@
 declare const window: any;
 export const environment = {
-  production: true,
-  apiUrl: window.__env?.API_URL || '/' // Fallback ke proxy
+  production: false,
+  apiUrl: window.__env?.BASE_URL || `http://${window.location.hostname}:3000`,
+  endpoints: {
+    auth: {
+      base: 'auth/current',
+      login: 'auth/login',
+      register: 'auth/register',
+      refreshToken: 'auth/token',
+      resetPasswordRequest: 'auth/request-reset-password',
+      accountVerificationRequest: 'auth/resend-verification',
+      resetPassword: 'auth/reset-password',
+      updateEmailRequest: 'auth/update-email',
+      updatePassword: 'auth/update-password',
+    },
+  },
 };
