2a85c6a5d5a122e3939d8e7d624ccf4d3a27b8df
pangaribowo <fatahillahalifp04@gmail.com>
2025-03-06 06:11:23 +0700

    chore: update Dockerfile, entrypoint, environment handler, add workflows

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,11 +7,8 @@ if [ -z "$API_URL" ]; then
   exit 1
 fi
 
-# Envsubst ganti $API_URL ke Nginx
-envsubst '$API_URL' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
-
 # Inject ke env.js
-echo "window.__env = { API_URL: '$API_URL' };" > /usr/share/nginx/html/assets/env.js
+echo "window.__env = { API_URL: '$API_URL' };" > /usr/share/nginx/html/assets/env-7JDVT4U3.js
 
 # Jalankan Nginx di foreground
 exec nginx -g "daemon off;"
